5de9cdc3cff8cbd23179ce74ee5e8786f3fda5a0
Pamella Gaiguer Santos <pamella.santos@totvs.com.br>
2015-11-16 11:37:50 -0200

    formulario para solicitacao de poc

---
+++ portalPoc/forms/SolicitacaoPoc/js/custom.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+
+  var calendario = FLUIGC.calendar('#desiredTime');
+  $("#envolvedValue").mask("#.##0,00", {reverse: true});
+  //$("#desiredTime").mask("00/00/0000");
+
+});
